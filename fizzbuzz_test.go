9298b2a84e52fa6206903e064ca00c7d66d59726
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2019-07-21 03:20:37 +0900

    add structure for test case

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -3,25 +3,29 @@ package tdd
 import "testing"
 
 func Test_Stringify(t *testing.T) {
-	t.Run("when number is 1", func(t *testing.T) {
-		if actual, expect := Stringify(1), "1"; actual != expect {
-			t.Errorf("actual=%s, expect=%s", actual, expect)
-		}
-	})
-	t.Run("when number is 2", func(t *testing.T) {
-		if actual, expect := Stringify(2), "2"; actual != expect {
-			t.Errorf("actual=%s, expect=%s", actual, expect)
-		}
-	})
-	t.Run("when number is 100", func(t *testing.T) {
-		if actual, expect := Stringify(100), "100"; actual != expect {
-			t.Errorf("actual=%s, expect=%s", actual, expect)
-		}
+	t.Run("normal number", func(t *testing.T) {
+		t.Run("when number is 1", func(t *testing.T) {
+			if actual, expect := Stringify(1), "1"; actual != expect {
+				t.Errorf("actual=%s, expect=%s", actual, expect)
+			}
+		})
+		t.Run("when number is 2", func(t *testing.T) {
+			if actual, expect := Stringify(2), "2"; actual != expect {
+				t.Errorf("actual=%s, expect=%s", actual, expect)
+			}
+		})
+		t.Run("when number is 100", func(t *testing.T) {
+			if actual, expect := Stringify(100), "100"; actual != expect {
+				t.Errorf("actual=%s, expect=%s", actual, expect)
+			}
+		})
 	})
 
-	t.Run("when number is 3", func(t *testing.T) {
-		if actual, expect := Stringify(3), "fizz"; actual != expect {
-			t.Errorf("actual=%s, expect=%s", actual, expect)
-		}
+	t.Run("Multiple of 3", func(t *testing.T) {
+		t.Run("when number is 3", func(t *testing.T) {
+			if actual, expect := Stringify(3), "fizz"; actual != expect {
+				t.Errorf("actual=%s, expect=%s", actual, expect)
+			}
+		})
 	})
 }
