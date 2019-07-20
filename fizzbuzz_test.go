56199d51567ad79335f77d08bb1d9dbc198a6786
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2019-07-21 03:45:26 +0900

    add table test case

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -2,49 +2,53 @@ package tdd
 
 import "testing"
 
+type TestCase struct {
+	description string
+	input       int
+	output      string
+}
+
 func Test_Stringify(t *testing.T) {
 	t.Run("normal number", func(t *testing.T) {
-		t.Run("when number is 1", func(t *testing.T) {
-			if actual, expect := Stringify(1), "1"; actual != expect {
-				t.Errorf("actual=%s, expect=%s", actual, expect)
-			}
-		})
-		t.Run("when number is 2", func(t *testing.T) {
-			if actual, expect := Stringify(2), "2"; actual != expect {
-				t.Errorf("actual=%s, expect=%s", actual, expect)
-			}
-		})
-		t.Run("when number is 101", func(t *testing.T) {
-			if actual, expect := Stringify(101), "101"; actual != expect {
-				t.Errorf("actual=%s, expect=%s", actual, expect)
-			}
-		})
+		cases := []TestCase{
+			{"number is 1", 1, "1"},
+			{"number is 101", 101, "101"},
+		}
+		for _, c := range cases {
+			t.Run(c.description, func(t *testing.T) {
+				if actual, expect := Stringify(c.input), c.output; actual != expect {
+					t.Errorf("actual=%s, expect=%s", actual, expect)
+				}
+			})
+		}
 	})
 
 	t.Run("Multiple of 3", func(t *testing.T) {
-		t.Run("when number is 3", func(t *testing.T) {
-			if actual, expect := Stringify(3), "fizz"; actual != expect {
-				t.Errorf("actual=%s, expect=%s", actual, expect)
-			}
-		})
-		t.Run("when number is 99", func(t *testing.T) {
-			if actual, expect := Stringify(99), "fizz"; actual != expect {
-				t.Errorf("actual=%s, expect=%s", actual, expect)
-			}
-		})
+		cases := []TestCase{
+			{"number is 3", 3, "fizz"},
+			{"number is 99", 99, "fizz"},
+		}
+		for _, c := range cases {
+			t.Run(c.description, func(t *testing.T) {
+				if actual, expect := Stringify(c.input), c.output; actual != expect {
+					t.Errorf("actual=%s, expect=%s", actual, expect)
+				}
+			})
+		}
 	})
 
 	t.Run("Multiple of 5", func(t *testing.T) {
-		t.Run("when number is 5", func(t *testing.T) {
-			if actual, expect := Stringify(5), "buzz"; actual != expect {
-				t.Errorf("actual=%s, expect=%s", actual, expect)
-			}
-		})
-		t.Run("when number is 100", func(t *testing.T) {
-			if actual, expect := Stringify(100), "buzz"; actual != expect {
-				t.Errorf("actual=%s, expect=%s", actual, expect)
-			}
-		})
+		cases := []TestCase{
+			{"number is 5", 5, "buzz"},
+			{"number is 100", 100, "buzz"},
+		}
+		for _, c := range cases {
+			t.Run(c.description, func(t *testing.T) {
+				if actual, expect := Stringify(c.input), c.output; actual != expect {
+					t.Errorf("actual=%s, expect=%s", actual, expect)
+				}
+			})
+		}
 	})
 
 }
