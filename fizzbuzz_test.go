79da3bf95d955db49d10de3d93bdddd714a1e9b6
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2019-07-21 03:02:46 +0900

    add some test case

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -3,7 +3,19 @@ package tdd
 import "testing"
 
 func Test_Stringify(t *testing.T) {
-	if actual, expect := Stringify(1), "1"; actual != expect {
-		t.Errorf("actual=%s, expect=%s", actual, expect)
-	}
+	t.Run("when number is 1", func(t *testing.T) {
+		if actual, expect := Stringify(1), "1"; actual != expect {
+			t.Errorf("actual=%s, expect=%s", actual, expect)
+		}
+	})
+	t.Run("when number is 2", func(t *testing.T) {
+		if actual, expect := Stringify(2), "2"; actual != expect {
+			t.Errorf("actual=%s, expect=%s", actual, expect)
+		}
+	})
+	t.Run("when number is 100", func(t *testing.T) {
+		if actual, expect := Stringify(100), "100"; actual != expect {
+			t.Errorf("actual=%s, expect=%s", actual, expect)
+		}
+	})
 }
